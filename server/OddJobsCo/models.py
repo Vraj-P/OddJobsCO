dff31d9720056f8e39439b7c4954bedbe10a5d82
VInay <v5chaudh@uwaterloo.ca>
2023-03-29 22:55:15 -0400

    added password field to user model

--- server/OddJobsCo/models.py
+++ server/OddJobsCo/models.py
@@ -8,6 +8,7 @@ class User(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=120)
     email = models.EmailField(max_length=120)
+    password = models.CharField(max_length=128)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
     bio = models.TextField()
