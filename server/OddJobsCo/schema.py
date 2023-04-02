b74ab1fa2b55c2fcc7b8095b00c4fca853277325
Vraj Patel <vrpatel8725@gmail.com>
2023-04-02 17:35:12 -0400

    adds update and delete functionality

--- server/OddJobsCo/schema.py
+++ server/OddJobsCo/schema.py
@@ -65,9 +65,61 @@ class CreateListing(graphene.Mutation):
             price=listing.price
         )
     
+class UpdateListing(graphene.Mutation):
+    id = graphene.Int()
+    title = graphene.String()
+    description = graphene.String()
+    completed = graphene.Boolean()
+    price = graphene.Float()
+    listings = graphene.List(ListingType)
+
+    class Arguments:
+        id = graphene.ID()
+        title = graphene.String()
+        description = graphene.String()
+        completed = graphene.Boolean()
+        price = graphene.Float()
+        user_id = graphene.ID()
+
+    @login_required
+    def mutate(self, info, id, title, description, completed, price, user_id):
+        user = User.objects.get(id=user_id)
+        listing = Listing.objects.get(id=id)
+        listing.title = title
+        listing.description = description
+        listing.completed = completed
+        listing.price = price
+        listing.user = user
+        listing.save()
+
+        return UpdateListing(
+            id=listing.id,
+            title=listing.title,
+            description=listing.description,
+            completed=listing.completed,
+            price=listing.price
+        )
+    
+class DeleteListing(graphene.Mutation):
+    id = graphene.ID()
+    listings = graphene.List(ListingType)
+
+    class Arguments:
+        id = graphene.ID()
+
+    @login_required
+    def mutate(self, info, id):
+        listing = Listing.objects.get(id=id)
+        listing.delete()
+
+        return DeleteListing(
+            id=listing.id
+        )
 
 class Mutation(graphene.ObjectType):
     create_listing = CreateListing.Field()
+    update_listing = UpdateListing.Field()
+    delete_listing = DeleteListing.Field()
 
 
 
